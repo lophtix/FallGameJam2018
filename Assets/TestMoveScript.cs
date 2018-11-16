f146a88c105da8207f7f5bc21e83726840822ec0
Benjamin Sundvall <benjamin@urgott.se>
2018-11-16 20:45:53 +0100

    Response on github test

--- Assets/TestMoveScript.cs
+++ Assets/TestMoveScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyInput : MonoBehaviour {
+public class TestMoveScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
