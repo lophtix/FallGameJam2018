9c1f6c9925e9d2d136b8d81cd8e62aef22001e7b
Benjamin Sundvall <benjamin@urgott.se>
2018-11-16 21:34:57 +0100

    Changed movement input

--- Assets/Scripts/KeyInput.cs
+++ Assets/Scripts/KeyInput.cs
@@ -17,20 +17,7 @@ public class KeyInput : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.W)){
-            movement.up = 1;
-		} else if (Input.GetKey(KeyCode.S)){
-            movement.up = -1;
-		} else {
-            movement.up = 0;
-		}
-		if (Input.GetKey(KeyCode.D)){
-            movement.right = 1;
-		} else if (Input.GetKey(KeyCode.A)){
-            movement.right = -1;
-		} else {
-            movement.right = 0;
-		}
-
+        movement.up = Input.GetAxis("Vertical");
+        movement.right = Input.GetAxis("Horizontal");
 	}
 }
