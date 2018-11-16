a86dce0a1a756067474244f415302e5c205de9f2
Jonatan Wästlund <jonte.w1@gmail.com>
2018-11-16 20:57:06 +0100

    Update KeyInput.cs

--- Assets/Scripts/KeyInput.cs
+++ Assets/Scripts/KeyInput.cs
@@ -1,16 +1,31 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public GameObject Player; 
 
-public class KeyInput : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Player = GameObject.Find("MainCharacter");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKey(KeyCode("W"))){
+			Player.up = 1;
+		} else if (Input.GetKey(KeyCode("S"))){
+			Player.up = -1;
+		} else {
+			Player.up = 0;
+		} 
+		if (Input.GetKey(KeyCode("D"))){
+			Player.right = 1;
+		} else if (Input.GetKey(KeyCode("A"))){
+			Player.right = -1;
+		} else {
+			Player.right = 0;
+		}
 		
 	}
 }
