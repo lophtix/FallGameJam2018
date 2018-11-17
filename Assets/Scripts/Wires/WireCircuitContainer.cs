6248f993d057236a6ccde752b4ccc0bb4fe208aa
Johan F <johan.furuhjelm@gmail.com>
2018-11-17 13:05:39 +0100

    Added prefabs

--- Assets/Scripts/Wires/WireCircuitContainer.cs
+++ Assets/Scripts/Wires/WireCircuitContainer.cs
@@ -24,7 +24,6 @@ public class WireCircuitContainer : LogicWire {
             foreach(Wire wire in wires)
             {
                 wire.setStatus(state);
-                Debug.Log(state);
             }
             currentState = state;
         }
