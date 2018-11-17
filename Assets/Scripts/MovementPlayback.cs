558bb5df1fb51786498a413326a850c0be536595
Benjamin Sundvall <benjamin@urgott.se>
2018-11-17 14:31:16 +0100

    Update MovementPlayback.cs

--- Assets/Scripts/MovementPlayback.cs
+++ Assets/Scripts/MovementPlayback.cs
@@ -5,27 +5,34 @@ using UnityEngine;
 public class MovementPlayback : MonoBehaviour {
     private GameObject clock;
     private ClockScript time;
-    private int tick;
+    private int myTick;
     private int loop;
 
     private GameObject player;
 
     List<List<Vector3>> cloneArrays;
-	// Use this for initialization
-	void Start () {
-        player = GameObject.Find("MainCharacter");
-        cloneArrays = player.GetComponent<MovementRecorder>().getCloneArrays();
+    MovementRecorder cloneArraysX;
 
+    // Use this for initialization
+    void Start () {
+        print("start");
+        player = GameObject.Find("MainCharacter");
+        print("a" + player);
+        cloneArraysX = player.GetComponent<MovementRecorder>();
+        print("ca1");
+        cloneArrays = cloneArraysX.getCloneArrays();
+        print("ca" + cloneArrays);
         clock = GameObject.Find("Clock");
         time = clock.GetComponent<ClockScript>();
+        print("time" + time);
     }
 
     private void FixedUpdate()
     {
-        tick = time.tick;
+        myTick = time.tick;
         if(time.loop > 0)
         {
-            transform.position = cloneArrays[time.loop - 1][tick];
+            transform.position = cloneArrays[time.loop - 1][myTick];
         }
     }
 }
