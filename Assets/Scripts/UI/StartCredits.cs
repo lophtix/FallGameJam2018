5aab855c1afe1b5722561651e7650bcf56bc5354
Johan F <johan.furuhjelm@gmail.com>
2018-11-17 22:11:19 +0100

    Added crecits

---
+++ Assets/Scripts/UI/StartCredits.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCredits : MonoBehaviour {
+
+    public GameObject creditCanvas, menuCanvas;
+
+	public void startCredits()
+    {
+        creditCanvas.SetActive(true);
+        menuCanvas.SetActive(false);
+    }
+
+    public void quitCredits()
+    {
+        creditCanvas.SetActive(false);
+        menuCanvas.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            quitCredits();
+        }
+    }
+}
