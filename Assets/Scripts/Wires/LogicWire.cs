0ced1dac73fa21f0428e67b6aab7b78e66dcd411
Johan F <johan.furuhjelm@gmail.com>
2018-11-18 11:23:27 +0100

    Removed changes to logicWires

--- Assets/Scripts/Wires/LogicWire.cs
+++ Assets/Scripts/Wires/LogicWire.cs
@@ -7,13 +7,13 @@ public abstract class LogicWire : MonoBehaviour {
     public GameObject[] Outputs;
     public GameObject[] Inputs;
 
-    public void Start()
-    {
-        foreach(GameObject obj in Outputs)
-        {
+    //public void Start()
+    //{
+    //    foreach(GameObject obj in Outputs)
+    //    {
 
-        }
-    }
+    //    }
+    //}
 
     public abstract void setStatus(bool status);
 
