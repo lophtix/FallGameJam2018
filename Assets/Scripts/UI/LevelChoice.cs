35a2d1ab355aea2515f5c3ed679bbe7ad194e7fd
Johan F <johan.furuhjelm@gmail.com>
2018-11-18 11:53:09 +0100

    Level Select added

---
+++ Assets/Scripts/UI/LevelChoice.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChoice : MonoBehaviour {
+
+    public int levelToSwitchTo;
+
+    public void switchToLevel()
+    {
+        SceneManager.LoadScene(levelToSwitchTo);
+    }
+}
